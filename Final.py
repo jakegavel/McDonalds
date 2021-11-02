6d29715edc02aae95a52ad6233807ca9174180a8
jakegavel <90931834+jakegavel@users.noreply.github.com>
2021-11-02 17:24:02 -0400

    Update Final.py

--- Final.py
+++ Final.py
@@ -98,7 +98,7 @@ def variants():
 
     dat["Rank"] = rank
     df = dat.dropna()
-    df = df.to_csv(path_or_buf = 'mcdonalds_clean1.csv', index=False)
+    df = pd.df.to_csv(path_or_buf = 'mcdonalds_clean1.csv', index=False)
 def maps(state_select):
 
     sl.title("McDonald's In Your Area")
